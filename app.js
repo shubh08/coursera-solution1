9f8853aa8d9aef1091b247da7f9e57cff249fdb2
Shubham Kumar <shubhamkumar567@gmail.com>
2016-11-18 16:46:16 +0530

    Assignment 1 of Coursera

---
+++ app.js
@@ -0,0 +1,37 @@
+(function () {
+
+angular.module('lunch-checker',[])
+
+.controller('lunch_controller', function ($scope) {
+$scope.message="The input tab is empty right now!!";
+
+
+$scope.lunch_checkerfun = function ()
+{
+if($scope.lunch=='null')
+{
+$scope.message="Empty";
+
+}
+var length=$scope.lunch.split(',').length;
+
+if((length<=3)&&(length>=1))
+  $scope.message="Enjoy!!";
+
+  else if(length>=3)
+  $scope.message="Too Much!!";
+
+  else {
+    $scope.message="Empty";
+  }
+
+
+}
+
+
+
+}
+
+
+)
+})();
